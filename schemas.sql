52e77a92eca03c8ee6272125c164dfc3c314240b
Noriega402 <d.noriegachajon@gmail.com>
2022-11-23 21:43:19 -0600

    Add file to create schemas with querys

---
+++ schemas.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE company;
+
+CREATE TABLE public.employees
+(
+    employee_id serial NOT NULL,
+    employee_name character varying(60) NOT NULL,
+    employee_surname character varying(60),
+    employee_birthday date NOT NULL,
+    employee_gender integer NOT NULL,
+    employee_salary numeric(8, 2),
+    position_name character varying(50),
+    created_at date,
+    departament_id integer NOT NULL,
+    PRIMARY KEY (employee_id)
+);
+
+ALTER TABLE IF EXISTS public.employees
+    OWNER to postgres;
+	
+
+CREATE TABLE public.departaments
+(
+    id serial NOT NULL,
+    departament_name character varying(75),
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE IF EXISTS public.departaments
+    OWNER to postgres;
+	
+ALTER TABLE IF EXISTS public.employees
+ADD CONSTRAINT departaments_employee_fk FOREIGN KEY (departament_id)
+REFERENCES public.departaments (id) MATCH SIMPLE
+ON UPDATE NO ACTION
+ON DELETE CASCADE
+NOT VALID;
